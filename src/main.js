4df7858ccd3c68f7f2efb498ec0f4a124636c0c3
Radhi Hudijan <radhif2009@gmail.com>
2023-10-29 12:18:32 +0100

    bootstrap vue set up completed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+//importing custom file for scss
+import "./custom.scss";
+
+//Importing Bootstrap and BootstrapVue CSS files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.config.productionTip = false;
+
+//Make BootstrapVue & icon plugin available for the project
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");

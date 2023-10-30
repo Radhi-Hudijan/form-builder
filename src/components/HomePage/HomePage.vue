b4d5608ca202455a0218783801442cb490577d82
Radhi Hudijan <radhif2009@gmail.com>
2023-10-30 14:19:16 +0100

    created question card

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="home">
     <nav-bar @save-form="handleSaveForm" />
+
+    <div class="homeWrapper">
+      <div class="sideBar">
+        <div class="questionList">
+          <question-card
+            title="what is your name"
+            type="short test"
+            @delete-card="handleDeleteCard"
+            @card-click="handleCardClick"
+          />
+        </div>
+        <div class="dropDownList">
+          <b-dropdown id="dropdown-1" block text="Add Question" class="m-md-2">
+            <b-dropdown-item>Long Text</b-dropdown-item>
+            <b-dropdown-item>Short Text</b-dropdown-item>
+          </b-dropdown>
+        </div>
+      </div>
+
+      <div class="homeContent">homeContent</div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from "../NavBar/NavBar.vue";
+import QuestionCard from "../QuestionCard/QuestionCard.vue";
+
 export default {
   name: "HomePage",
   components: {
     NavBar,
+    QuestionCard,
   },
 
   methods: {
     handleSaveForm() {
       console.log("The Form has been saved successfully");
+    },
+    handleDeleteCard() {
+      console.log("The card has been deleted successfully");
+    },
+    handleCardClick() {
+      console.log("card clicked");
     },
   },
 };
---
+++ src/components/QuestionCard/QuestionCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <b-card
+      :title="title"
+      :sub-title="type"
+      class="border-0 card-content"
+      @click="handleCardClick()"
+    />
+    <b-icon icon="trash" @click="handleDeleteCard()" class="trash-icon" />
+  </div>
+</template>
+
+<script>
+import { BCard, BIcon } from "bootstrap-vue";
+export default {
+  name: "QuestionCard",
+  components: { BCard, BIcon },
+  props: ["title", "type"],
+  emits: ["delete-card", "card-click"],
+  methods: {
+    handleDeleteCard() {
+      this.$emit("delete-card");
+    },
+
+    handleCardClick() {
+      this.$emit("card-click");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
